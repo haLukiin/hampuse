f4f24f47437ef890036feb237c91963b25e7f240
unknown <sspel25hula@axevalla.fhsk.se>
2025-09-30 11:48:58 +0200

    30/9

--- Uppgift 03 - Operander och beräkningar/uppgift 3/uppgift 3/Program.cs
+++ Uppgift 03 - Operander och beräkningar/uppgift 3/uppgift 3/Program.cs
@@ -11,22 +11,13 @@ namespace uppgift_3
     internal class Program
     {
         
-
-
-        static void Greet(string name)
-        {
-
-            Console.WriteLine("Hello " );
-        }
-
-
         static void Main(string[] args)
         {
 
             int current_year = 2025;
             int birth_year = 0;
             Console.WriteLine("Hello, whats your name?");
-             string name = Console.ReadLine();
+            string name = Console.ReadLine();
             Console.WriteLine("whats ur age?");
             int age;
             age = Convert.ToInt32(Console.ReadLine());         
